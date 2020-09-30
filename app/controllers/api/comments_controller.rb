21802d57e3648a1308f859a90927e7e9132fec91
Braxton McClellan <thebraxmc@gmail.com>
2020-09-29 18:38:17 -0600

    backend done

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,40 @@
+class Api::CommentsController < ApplicationController
+  before_action :set_post
+
+  def index
+    render json: @post.comments
+  end
+
+  def create
+    comment = @post.comments.new(comment_params)
+    if comment.save
+      render json: comment
+    else
+      render json: { errors: comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    comment = @post.comment.find(params[:id]
+    if comment.update
+      render json: comment
+    else
+      render json: { errors: comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.comments.find(params[:id]).destroy
+    render json: { message: 'Comment Deleted'}
+  end
+
+  private
+    def set_post
+     @post = Post.find(params[:post])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:author, :subject, :body)
+    end
+
+end
